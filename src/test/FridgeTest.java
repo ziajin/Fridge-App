c093eea44158ddb208b8f746d7d7c8eefea4e542
Zia Jin <zijin15@students.cs.ubc.ca>
2025-10-08 14:25:45 -0700

    Added tests for Fridge class

---
+++ src/test/FridgeTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class FridgeTest {
+
+
+    @BeforeEach 
+    public void BeforeEach() {
+        //stub
+    }
+
+    @Test
+    public void TestAddItem() {
+        //stub
+    }
+
+    @Test
+    public void TestRemoveItem() {
+        //stub
+    }
+    
+    @Test
+    public void TestExpriryDaTe() {
+        //stub
+    }
+
+    @Test
+    public void TestChangeQuantity() {
+        //stub
+    }
+
+
+    
+}
