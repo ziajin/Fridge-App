d039fdbe929e8bce6a9ec052e43974fa6d9c3d98
Zia Jin <zijin15@students.cs.ubc.ca>
2025-10-08 22:53:05 -0700

    Implemented Food Class

---
+++ src/main/model/Food.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Food {
+
+    protected int quantity;
+    protected int expiryDate;
+    protected String name;
+
+
+    public Food (String name, int expiryDate, int quantity) {
+        this.name = name;
+        this.expiryDate = expiryDate;
+        this.quantity = quantity;
+    }
+
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(int expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
