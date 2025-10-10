5779c162ca210a0f632ea495b610a3f909c6bf24
Zia Jin <zijin15@students.cs.ubc.ca>
2025-10-10 16:44:04 -0700

    Implemented fruit class

--- src/main/model/Fruit.java
+++ src/main/model/Fruit.java
@@ -10,7 +10,7 @@ public class Fruit extends Food {
     private boolean ripe;
 
     public Fruit(String name, int quantity, int expiryDate, boolean ripe) {
-        super(name, expiryDate, quantity);
+        super(name, quantity, expiryDate);
         this.ripe = ripe;
         determineExpiry(ripe);
     }
@@ -19,16 +19,22 @@ public class Fruit extends Food {
         return ripe;
     }
 
+    public void setRipe(boolean ripe) {
+        this.ripe = ripe;
+        determineExpiry(ripe);
+    }
+
     /*
      * REQUIRES: valid parameter value
      * MODIFIES: this
      * EFFECTS: if the fruit is ripe, return 3 days, if not return 6 days
      */
-    private int determineExpiry(boolean ripe) {
-        //stub
-        return 0;
+    private void determineExpiry(boolean ripe) {
+        if (ripe) {
+            setExpiryDate(2);
+        } else {
+            setExpiryDate(5);
+        }
     }
-
-
 
 }
