542c6b646971858c75177c331cbd7c8aa5a2f447
Zia Jin <zijin15@students.cs.ubc.ca>
2025-10-22 00:48:38 -0700

    Added coverage exemption annotation

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,5 +1,8 @@
 package ui;
 
+import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
+
+@ExcludeFromJacocoGeneratedReport
 public class Main {
     public static void main(String[] args) throws Exception {
 
