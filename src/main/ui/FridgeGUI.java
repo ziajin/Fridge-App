4e5c4fac607daeb730cc8eb1d38f5d00953e820c
Zia Jin <zijin15@students.cs.ubc.ca>
2025-11-12 00:26:31 -0800

    Added sp-ecification for FridgeGUI

---
+++ src/main/ui/FridgeGUI.java
@@ -0,0 +1,68 @@
+package ui;
+
+import java.awt.*;
+import javax.swing.*;
+
+import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
+
+import java.awt.event.*;
+import model.*;
+
+/*
+ * FridgeGUI class creates a Swing GUI that handles the user input and displays options in a 
+ * window. Allows user to interact with the digital fridge.
+ */
+
+ @ExcludeFromJacocoGeneratedReport
+
+public class FridgeGUI extends JFrame {
+
+    public FridgeGUI() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: sets main menu before fridge interface. prompts user to load saved fridge or not
+    private void mainMenu() {
+
+    }
+
+    //EFFECTS: sets up and assigns actions to buttons
+    private void setButtons() {
+
+    }
+
+    //EFFECTS: sets up text area to display fridge contents
+    private void setFridgeDisplay() {
+        
+    }
+
+    //MODIFIES: this
+    //EFFECTS: prompts user for values, creates food, adds to fridge
+    private void addItem() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: removes specified qauntity from specified item in fridge. if all quantities of the 
+    // item is removed, remove the item entirely.
+    private void removeItem() {
+
+    }
+
+    //EFFECTS: prints out contents of fridge onto panel
+    private void viewFridge() {
+
+    }
+
+    // EFFECTS: prints out contents of freezer onto panel
+    private void viewFreezer() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: changes item's expiry date
+    private void changeExpiryDate() {
+
+    }
+}
